a7f066c734baaadf7242dd50b4fc90005dd3a44b
USUASUS\sidne <sidneyshane_dizon@yahoo.com>
2018-03-08 08:41:45 -0700

    Sidney: created the source code.

---
+++ src/AnalyzerAssignment1.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ * @author Sidney Shane Dizon, Sunah Kim, Kristopher Tang
+ * 
+ * This is a java appplication analysis tool that will find the number of
+ * occurences of the declarations of and references to a Java type (as a 
+ * fully qualified name, like java.lang.String), within a specified 
+ * directory.
+ *
+ *	Input:
+ * This takes in pathname to indicate a directory of interest.
+ * Takes a string to indicate a fully qualified name of a Java type.
+ *	
+ *	Process:
+ * The java application counts the number of declarations of that type
+ * within that directory and counts that number of references to each 
+ * occurrence of that type within that directory.
+ * 
+ *	Output:
+ * The output is a string written to the console stating the name of the 
+ * type, the count of declarations, and the count of references, such as:
+ * 
+ * 		<type>. Declarations found: <count>; references found: <count>.
+ * 
+ * where the <type> and two <count>s are to be replaced with the relevant 
+ * values.
+ */
+
+public class AnalyzerAssignment1 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
