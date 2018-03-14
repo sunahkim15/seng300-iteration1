7283074fcba46d9bf1b81f7daf23c2249982614a
sunahkim15 <sunah.kim@ucalgary.ca>
2018-03-13 21:14:59 -0600

    added to TestParse

--- src/assignment/TestParse.java
+++ src/assignment/TestParse.java
@@ -2,13 +2,32 @@ package assignment;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestParse {
+	private static String BASEDIR = "C:\\Users\\esthe\\Desktop\\test300assign"; 
+	String[] sourcePath = {BASEDIR};
+	
+	File file = new File(BASEDIR + "\\dummy.java");
+	
+	
+	@Before
+	public void setup() throws IOException {
+		FileWriter writer = new FileWriter(file);
+		writer.write("public class Dummy {\n public int a;\n public void method(){}\n}");
+		writer.close();
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test() throws FileNotFoundException, IOException {
+		String filecontent = AnalyzerAssignment1.readFileToString(BASEDIR + "\\dummy.java");
+		AnalyzerAssignment1.parse(filecontent, sourcePath, "dummy.java", "int");
 	}
 
 }
